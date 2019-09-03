dd0f7924b8f4173adc983f4ad36c8b76ed8074fe
Carmen Chapa <cmchapa28@gmail.com>
2019-09-03 17:54:04 +0200

    Load images and fonts from expo

--- aluxion-test/App.js
+++ aluxion-test/App.js
@@ -1,17 +1,55 @@
 import React from "react"
 import {StyleSheet, Text, View} from "react-native"
 import AppContainer from "./src/Navigator"
+import {AppLoading} from "expo"
+import * as Font from "expo-font"
+import Images from "./src/Images"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <StatusBar
-        backgroundColor={s.smoke_white.color}
-        barStyle="dark-content"
-      /> */}
-      <AppContainer uriPrefix="/app" />
-    </View>
-  )
+export default class App extends React.Component {
+  state = {
+    isReady: false
+  }
+
+  _cacheResourcesAsync = (): Promise<*> => {
+    const asyncTasks = Images.downloadAsync().concat([
+      Font.loadAsync({
+        MuseoLight: require("./Resources/fonts/MuseoSans-100.otf"),
+        MuseoRegular: require("./Resources/fonts/MuseoSans-300.otf"),
+        MuseoMedium: require("./Resources/fonts/MuseoSans-500.otf"),
+        MuseoBold: require("./Resources/fonts/MuseoSans-700.otf"),
+        MuseoBlack: require("./Resources/fonts/MuseoSans-900.otf"),
+        MuseoItalicLight: require("./Resources/fonts/MuseoSans-100Italic.otf"),
+        MuseoItalicRegular: require("./Resources/fonts/MuseoSans-300Italic.otf"),
+        MuseoItalicMedium: require("./Resources/fonts/MuseoSans-500Italic.otf"),
+        MuseoItalicBold: require("./Resources/fonts/MuseoSans-700Italic.otf"),
+        MuseoItalicBlack: require("./Resources/fonts/MuseoSans-900Italic.otf")
+      })
+    ])
+
+    return Promise.all(asyncTasks).catch(error => console.log(error))
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({isReady: true})}
+          onError={console.warn}
+        />
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+        {/* <StatusBar
+	        backgroundColor={s.smoke_white.color}
+	        barStyle="dark-content"
+	      /> */}
+        <AppContainer uriPrefix="/app" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ aluxion-test/src/Images.js
@@ -0,0 +1,23 @@
+import {Asset} from "expo-asset"
+
+export default class Images {
+  static menu = require("../Resources/icons/ic_menu/ic_menu_black.png")
+  static close = require("../Resources/icons/ic_close/ic_close_black.png")
+  static close2 = require("../Resources/icons/ic_close/ic_close_black2x.png")
+  static close3 = require("../Resources/icons/ic_close/ic_close_black3x.png")
+  static closeWhite = require("../Resources/icons/ic_close/ic_close_white.png")
+  static closeWhite2 = require("../Resources/icons/ic_close/ic_close_white2x.png")
+  static closeWhite3 = require("../Resources/icons/ic_close/ic_close_white3x.png")
+
+  static downloadAsync(): Promise<*>[] {
+    return [
+      Asset.fromModule(Images.menu).downloadAsync(),
+      Asset.fromModule(Images.close).downloadAsync(),
+      Asset.fromModule(Images.close2).downloadAsync(),
+      Asset.fromModule(Images.close3).downloadAsync(),
+      Asset.fromModule(Images.closeWhite).downloadAsync(),
+      Asset.fromModule(Images.closeWhite2).downloadAsync(),
+      Asset.fromModule(Images.closeWhite3).downloadAsync()
+    ]
+  }
+}
