3a023eb108a505d88ce368175b170d61993cb265
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-04 15:36:39 +0200

    Make stak a drawer stack

--- aluxion-test/src/Navigator.js
+++ aluxion-test/src/Navigator.js
@@ -1,40 +1,44 @@
-import {createAppContainer} from "react-navigation"
-import {createStackNavigator} from "react-navigation-stack"
-import FeeScreen from "./screens/Fee"
-import DetailScreen from "./screens/Detail"
-import ProfileScreen from "./screens/Profile"
+import { createAppContainer } from "react-navigation";
+// import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import FeeScreen from "./screens/Fee";
+import DetailScreen from "./screens/Detail";
+import ProfileScreen from "./screens/Profile";
 
-const RootNavigator = createStackNavigator(
+const RootNavigator = createDrawerNavigator(
   {
     Fee: {
       screen: FeeScreen,
 
-      navigationOptions: ({navigation}) => ({
+      navigationOptions: ({ navigation }) => ({
         title: `Fee`
+        // drawerLabel: () => null
       })
     },
     Detail: {
       screen: DetailScreen,
 
-      navigationOptions: ({navigation}) => ({
-        title: `Detail`
+      navigationOptions: ({ navigation }) => ({
+        title: `Detail`,
+        drawerLabel: () => null
       })
     },
     Profile: {
       screen: ProfileScreen,
 
-      navigationOptions: ({navigation}) => ({
-        title: `Profile`
+      navigationOptions: ({ navigation }) => ({
+        title: `Profile`,
+        drawerLabel: () => null
       })
     }
   },
   {
     initialRouteName: "Fee",
     headerMode: "none",
-    cardStyle: {backgroundColor: "#fff"}
+    cardStyle: { backgroundColor: "#fff" }
   }
-)
+);
 
-const AppContainer = createAppContainer(RootNavigator)
+const AppContainer = createAppContainer(RootNavigator);
 
-export default AppContainer
+export default AppContainer;
