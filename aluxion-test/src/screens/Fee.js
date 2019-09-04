936f2c306c19875378327173b109b9129b03c8f1
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-04 18:43:14 +0200

    Use ImageListItem

--- aluxion-test/src/screens/Fee.js
+++ aluxion-test/src/screens/Fee.js
@@ -13,6 +13,7 @@ import Unsplash from "unsplash-js/native";
 import { getPhotoTitle } from "../helperFunctions";
 import { styles as s } from "../Styles";
 import Header from "../components/Header";
+import { ImageListItem } from "../components/ImageList";
 
 const accesKey =
   "a2f508640cb62f314e0e0763594d40aab1c858a7ef796184067c537a88b276aa";
@@ -40,53 +41,59 @@ export default class FeeScreen extends React.Component {
       });
   }
 
-  isOdd = num => num % 2;
+  // isOdd = num => num % 2;
 
-  renderItem = ({ item, index }) => (
-    <TouchableOpacity
-      onPress={() => this.props.navigation.navigate("Detail", { image: item })}
-    >
-      <ImageBackground
-        style={[
-          s.listItem,
-          this.isOdd(index) ? { marginTop: 26 } : { marginBottom: 26 },
-          s.feeImageBackground
-        ]}
-        imageStyle={{ borderRadius: 10 }}
-        source={{
-          uri: item.urls.small
-        }}
-      >
-        <LinearGradient
-          colors={["transparent", "rgba(0,0,0,0.9)"]}
-          start={[0.5, 0.7]}
-          style={[s.listItem, s.gradient]}
-        />
-        <Text style={s.feePhotoTitle}>{getPhotoTitle(item)}</Text>
-        <Text style={s.feeLikes}>{`${item.likes} likes`}</Text>
-      </ImageBackground>
-    </TouchableOpacity>
-  );
+  // renderItem = ({ item, index, navigate }) => (
+  //   <TouchableOpacity
+  //     onPress={() => this.props.navigation.navigate("Detail", { image: item })}
+  //     // onPress={() => navigate()}
+  //   >
+  //     <ImageBackground
+  //       style={[
+  //         s.listItem,
+  //         this.isOdd(index) ? { marginTop: 26 } : { marginBottom: 26 },
+  //         s.feeImageBackground
+  //       ]}
+  //       imageStyle={{ borderRadius: 10 }}
+  //       source={{
+  //         uri: item.urls.small
+  //       }}
+  //     >
+  //       <LinearGradient
+  //         colors={["transparent", "rgba(0,0,0,0.9)"]}
+  //         start={[0.5, 0.7]}
+  //         style={[s.listItem, s.gradient]}
+  //       />
+  //       <Text style={s.feePhotoTitle}>{getPhotoTitle(item)}</Text>
+  //       <Text style={s.feeLikes}>{`${item.likes} likes`}</Text>
+  //     </ImageBackground>
+  //   </TouchableOpacity>
+  // );
 
   render() {
+    const { navigation } = this.props;
     return (
       <View style={s.container}>
         <FlatList
           numColumns={2}
           data={this.state.imgs}
           keyExtractor={(item, index) => item + index}
-          renderItem={this.renderItem}
-          columnWrapperStyle={{ flex: 1, justifyContent: "space-between" }}
+          // renderItem={this.renderItem}
+          renderItem={({ item, index }) => (
+            <ImageListItem
+              item={item}
+              index={index}
+              navigate={() => navigation.navigate("Detail", { image: item })}
+            />
+          )}
+          columnWrapperStyle={s.columnWrapperStyle}
           contentContainerStyle={{
             marginTop: 120,
             marginHorizontal: 26
           }}
         />
 
-        <Header
-          icon="menu"
-          onPress={() => this.props.navigation.openDrawer()}
-        />
+        <Header icon="menu" onPress={() => navigation.openDrawer()} />
 
         {/* <View style={s.headerContainer}>
           <View style={s.iconsContainer}>
