54684b0f766b187bfb08046983a4464838feeda0
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-04 21:00:16 +0200

    getFullName not used

--- aluxion-test/src/helperFunctions.js
+++ aluxion-test/src/helperFunctions.js
@@ -2,11 +2,11 @@ export function capitalizeFirst(item) {
   return item == null ? "" : item.charAt(0).toUpperCase() + item.slice(1);
 }
 
-export function getFullName(user) {
-  return `${capitalizeFirst(user.first_name)} ${capitalizeFirst(
-    user.last_name
-  )}`;
-}
+// export function getFullName(user) {
+//   return `${capitalizeFirst(user.first_name)} ${capitalizeFirst(
+//     user.last_name
+//   )}`;
+// }
 
 export function getPhotoTitle(item) {
   return item.description
