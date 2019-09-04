269e4adddbb256d48b825d133ad3f7716716a6b7
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-04 14:11:20 +0200

    Create helperFunctions

---
+++ aluxion-test/src/helperFunctions.js
@@ -0,0 +1,15 @@
+export function capitalizeFirst(item) {
+  return item == null ? "" : item.charAt(0).toUpperCase() + item.slice(1);
+}
+
+export function getFullName(user) {
+  return `${capitalizeFirst(user.first_name)} ${capitalizeFirst(
+    user.last_name
+  )}`;
+}
+
+export function getPhotoTitle(item) {
+  return item.description
+    ? capitalizeFirst(item.description)
+    : capitalizeFirst(item.alt_description);
+}
