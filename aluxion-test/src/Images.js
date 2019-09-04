d16cca882665a68956ab604a332934afd726ee74
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-04 14:10:28 +0200

    Remove flow type

--- aluxion-test/src/Images.js
+++ aluxion-test/src/Images.js
@@ -1,15 +1,15 @@
-import {Asset} from "expo-asset"
+import { Asset } from "expo-asset";
 
 export default class Images {
-  static menu = require("../Resources/icons/ic_menu/ic_menu_black.png")
-  static close = require("../Resources/icons/ic_close/ic_close_black.png")
-  static close2 = require("../Resources/icons/ic_close/ic_close_black2x.png")
-  static close3 = require("../Resources/icons/ic_close/ic_close_black3x.png")
-  static closeWhite = require("../Resources/icons/ic_close/ic_close_white.png")
-  static closeWhite2 = require("../Resources/icons/ic_close/ic_close_white2x.png")
-  static closeWhite3 = require("../Resources/icons/ic_close/ic_close_white3x.png")
+  static menu = require("../Resources/icons/ic_menu/ic_menu_black.png");
+  static close = require("../Resources/icons/ic_close/ic_close_black.png");
+  static close2 = require("../Resources/icons/ic_close/ic_close_black2x.png");
+  static close3 = require("../Resources/icons/ic_close/ic_close_black3x.png");
+  static closeWhite = require("../Resources/icons/ic_close/ic_close_white.png");
+  static closeWhite2 = require("../Resources/icons/ic_close/ic_close_white2x.png");
+  static closeWhite3 = require("../Resources/icons/ic_close/ic_close_white3x.png");
 
-  static downloadAsync(): Promise<*>[] {
+  static downloadAsync() {
     return [
       Asset.fromModule(Images.menu).downloadAsync(),
       Asset.fromModule(Images.close).downloadAsync(),
@@ -18,6 +18,6 @@ export default class Images {
       Asset.fromModule(Images.closeWhite).downloadAsync(),
       Asset.fromModule(Images.closeWhite2).downloadAsync(),
       Asset.fromModule(Images.closeWhite3).downloadAsync()
-    ]
+    ];
   }
 }
