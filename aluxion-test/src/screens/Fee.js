e569a226960e30066c990c264f452d87e74980d9
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-05 23:28:58 +0200

    Add header animations Add redux stuff Extract constants

--- aluxion-test/src/screens/Fee.js
+++ aluxion-test/src/screens/Fee.js
@@ -1,84 +1,105 @@
 import React from "react";
-import {
-  FlatList,
-  Image,
-  ImageBackground,
-  Text,
-  TouchableOpacity,
-  View
-} from "react-native";
-import Images from "../Images";
-import { LinearGradient } from "expo-linear-gradient";
-import Unsplash from "unsplash-js/native";
-import { getPhotoTitle } from "../helperFunctions";
+import { Animated, FlatList, View } from "react-native";
 import { styles as s } from "../Styles";
 import Header from "../components/Header";
 import { ImageListItem } from "../components/ImageList";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getRandomImages, imgsReducer } from "../redux/store";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { NAVBAR_HEIGHT, STATUS_BAR_HEIGHT } from "../Constants";
 
-const accesKey =
-  "a2f508640cb62f314e0e0763594d40aab1c858a7ef796184067c537a88b276aa";
-const secretKey =
-  "4ea19af370997bcb0c580c071437661346b073b8e2f5252871e171ecc3c783ee";
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
-const unsplash = new Unsplash({
-  applicationId: accesKey,
-  secret: secretKey
-});
-
-export default class FeeScreen extends React.Component {
+class FeeScreen extends React.Component {
+  scrollAnim = new Animated.Value(0);
+  offsetAnim = new Animated.Value(0);
   state = {
-    imgs: []
+    scrollAnim: this.scrollAnim,
+    offsetAnim: this.offsetAnim,
+    clampedScroll: Animated.diffClamp(
+      Animated.add(
+        this.scrollAnim.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 1],
+          extrapolateLeft: "clamp"
+        }),
+        this.offsetAnim
+      ),
+      0,
+      NAVBAR_HEIGHT - STATUS_BAR_HEIGHT
+    )
   };
+
+  _clampedScrollValue = 0;
+  _offsetValue = 0;
+  _scrollValue = 0;
+
   componentDidMount() {
-    unsplash.photos
-      .listPhotos(Math.floor(Math.random() * 100), 16, "random")
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ imgs: data });
-      })
-      .catch(err => {
-        console.log("Error happened during fetching!", err);
-      });
+    this.props.getImages();
+
+    this.state.scrollAnim.addListener(({ value }) => {
+      const diff = value - this._scrollValue;
+      this._scrollValue = value;
+      this._clampedScrollValue = Math.min(
+        Math.max(this._clampedScrollValue + diff, 0),
+        NAVBAR_HEIGHT - STATUS_BAR_HEIGHT
+      );
+    });
+    this.state.offsetAnim.addListener(({ value }) => {
+      this._offsetValue = value;
+    });
   }
 
-  // isOdd = num => num % 2;
+  componentWillUnmount() {
+    this.state.scrollAnim.removeAllListeners();
+    this.state.offsetAnim.removeAllListeners();
+  }
 
-  // renderItem = ({ item, index, navigate }) => (
-  //   <TouchableOpacity
-  //     onPress={() => this.props.navigation.navigate("Detail", { image: item })}
-  //     // onPress={() => navigate()}
-  //   >
-  //     <ImageBackground
-  //       style={[
-  //         s.listItem,
-  //         this.isOdd(index) ? { marginTop: 26 } : { marginBottom: 26 },
-  //         s.feeImageBackground
-  //       ]}
-  //       imageStyle={{ borderRadius: 10 }}
-  //       source={{
-  //         uri: item.urls.small
-  //       }}
-  //     >
-  //       <LinearGradient
-  //         colors={["transparent", "rgba(0,0,0,0.9)"]}
-  //         start={[0.5, 0.7]}
-  //         style={[s.listItem, s.gradient]}
-  //       />
-  //       <Text style={s.feePhotoTitle}>{getPhotoTitle(item)}</Text>
-  //       <Text style={s.feeLikes}>{`${item.likes} likes`}</Text>
-  //     </ImageBackground>
-  //   </TouchableOpacity>
-  // );
+  _onScrollEndDrag = () => {
+    this._scrollEndTimer = setTimeout(this._onMomentumScrollEnd, 250);
+  };
+
+  _onMomentumScrollBegin = () => {
+    clearTimeout(this._scrollEndTimer);
+  };
+
+  _onMomentumScrollEnd = () => {
+    const toValue =
+      this._scrollValue > NAVBAR_HEIGHT &&
+      this._clampedScrollValue > (NAVBAR_HEIGHT - STATUS_BAR_HEIGHT) / 2
+        ? this._offsetValue + NAVBAR_HEIGHT
+        : this._offsetValue - NAVBAR_HEIGHT;
+
+    Animated.timing(this.state.offsetAnim, {
+      toValue,
+      duration: 350,
+      useNativeDriver: true
+    }).start();
+  };
 
   render() {
+    console.log("props.images", this.props.images, this.props.images != null);
     const { navigation } = this.props;
+    const { clampedScroll } = this.state;
+
+    const navbarTranslate = clampedScroll.interpolate({
+      inputRange: [0, NAVBAR_HEIGHT - STATUS_BAR_HEIGHT],
+      outputRange: [0, -(NAVBAR_HEIGHT - STATUS_BAR_HEIGHT)],
+      extrapolate: "clamp"
+    });
+    const navbarOpacity = clampedScroll.interpolate({
+      inputRange: [0, NAVBAR_HEIGHT - STATUS_BAR_HEIGHT],
+      outputRange: [1, 0],
+      extrapolate: "clamp"
+    });
+    console.log(" navbarTranslate", navbarTranslate);
     return (
-      <View style={s.container}>
-        <FlatList
+      <View style={[s.flx1, s.centerContent]}>
+        <AnimatedFlatList
           numColumns={2}
-          data={this.state.imgs}
+          data={this.props.images.images}
           keyExtractor={(item, index) => item + index}
-          // renderItem={this.renderItem}
           renderItem={({ item, index }) => (
             <ImageListItem
               item={item}
@@ -88,21 +109,49 @@ export default class FeeScreen extends React.Component {
           )}
           columnWrapperStyle={s.columnWrapperStyle}
           contentContainerStyle={{
-            marginTop: 120,
+            marginTop: hp("14%"),
+            paddingTop: 10,
             marginHorizontal: 26
           }}
+          scrollEventThrottle={1}
+          onMomentumScrollBegin={this._onMomentumScrollBegin}
+          onMomentumScrollEnd={this._onMomentumScrollEnd}
+          onScrollEndDrag={this._onScrollEndDrag}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: this.state.scrollAnim } } }],
+            { useNativeDriver: true }
+          )}
         />
 
-        <Header icon="menu" onPress={() => navigation.openDrawer()} />
-
-        {/* <View style={s.headerContainer}>
-          <View style={s.iconsContainer}>
-            <Image source={Images.menu} />
-            <Text style={s.pageTitle}>Discover</Text>
-            <View style={{ width: 25 }} />
-          </View>
-        </View> */}
+        <Header
+          icon="menu"
+          onPress={() => navigation.openDrawer()}
+          extraStyles={[
+            s.header,
+            { transform: [{ translateY: navbarTranslate }] }
+          ]}
+          opacity={{ opacity: navbarOpacity }}
+        />
       </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  const { images } = state;
+  return { images };
+};
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getImages: getRandomImages,
+      imgsReducer
+    },
+    dispatch
+  );
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FeeScreen);
