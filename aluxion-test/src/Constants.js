70243d6c02ef5b697ac7b806b83539b02e970b36
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-06 01:51:24 +0200

    Add full screen constants

--- aluxion-test/src/Constants.js
+++ aluxion-test/src/Constants.js
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 import {
-  // widthPercentageToDP as wp,
+  widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
 
@@ -15,6 +15,8 @@ export const SECRET_KEY =
 
 export const NAVBAR_HEIGHT = hp("14%");
 export const STATUS_BAR_HEIGHT = Platform.select({ ios: 20, android: 24 });
+export const SCREEN_WIDTH = wp("100%");
+export const SCREEN_HEIGHT = hp("100%");
 export const HORIZONTAL_PADDING = 26;
 
 //-----** ActionTypes **-----//
